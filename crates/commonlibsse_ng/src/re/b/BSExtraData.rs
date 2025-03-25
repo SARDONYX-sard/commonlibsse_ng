193c5b2655690e3459f313f69032b069a10ca88d
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-03-26 02:27:02 +0900

    docs: add docs

--- crates/commonlibsse_ng/src/re/b/BSExtraData.rs
+++ crates/commonlibsse_ng/src/re/b/BSExtraData.rs
@@ -8,29 +8,44 @@ use core::ffi::c_void;
 use core::marker::PhantomData;
 use core::ptr::{self, NonNull};
 
+/// Represents the base structure for extra data nodes in a singly linked list.
 #[repr(C)]
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct BSExtraData {
+    /// Pointer to the virtual function table (vtable).
     pub vtbl: *const BSExtraDataVtbl,
-    /// This pointer is a unique pointer assumption that is made from the heap.
-    /// (e.g. Ptr from `Box::into_raw`)
+    /// Pointer to the next `BSExtraData` node.
     ///
-    /// Originally, `Option<NonNull<BSExtraData>>` would be the best choice, but since it is an FFI type, null must always be considered.
-    /// Therefore, a raw pointer is used.
+    /// This is a raw pointer to ensure FFI compatibility. `Option<NonNull<T>>`
+    /// would be safer, but raw pointers are required for interoperability.
+    /// # Note
+    /// This is assumed to be a heap-derived **unique pointer** created from `Box::into_raw`, etc.
+    ///
+    /// If this process is broken, UB happens.
     pub next: *mut BSExtraData,
+    /// Marker indicating that the pointer to next is owned and unique.
+    marker: PhantomData<BSExtraData>,
 }
+
 const _: () = assert!(core::mem::size_of::<BSExtraData>() == 0x10);
 
 impl BSExtraData {
+    /// Address & offset of RTTI for `BSExtraData`.
     pub const RTTI: VariantID = RTTI_BSExtraData;
+
+    /// Address & offset of Virtual function table.
     pub const VTABLE: [VariantID; 1] = VTABLE_BSExtraData;
+
+    /// Default extra data type (None).
     pub const EXTRA_DATA_TYPE: ExtraDataType = ExtraDataType::None;
 
+    /// Creates a new instance of `BSExtraData` with default values.
     #[inline]
     pub const fn new() -> Self {
-        Self { vtbl: &BS_EXTRA_DATA_VTBL, next: ptr::null_mut() }
+        Self { vtbl: &BS_EXTRA_DATA_VTBL, next: ptr::null_mut(), marker: PhantomData }
     }
 
+    /// Get the extra data type by invoking the `GetType` virtual function.
     #[inline]
     pub fn get_type(&self) -> ExtraDataType {
         let vtable = match Self::VTABLE[0].address() {
@@ -44,6 +59,7 @@ impl BSExtraData {
         unsafe { (vtable.as_ref().GetType)(self) }
     }
 
+    /// Compares two `BSExtraData` instances for inequality.
     #[inline]
     pub fn is_not_equal(&self, rhs: &Self) -> bool {
         let vtable = match Self::VTABLE[0].address() {
@@ -57,11 +73,18 @@ impl BSExtraData {
         unsafe { (vtable.as_ref().IsNotEqual)(self, rhs) }
     }
 
+    /// Creates a new instance of `T` that implements `CxxVirtClass`.
+    ///
+    /// Returns an `Option<NonNull<T>>` containing a pointer to the allocated instance, or `None` on failure.
     pub fn create<T: CxxVirtClass>() -> Option<NonNull<T>> {
         let t = &T::vtable()[0];
         Self::create_with(core::mem::size_of::<T>(), t.address().ok()?).map(|void| void.cast())
     }
 
+    /// Allocates and initializes a new `BSExtraData` instance with a given size and vtable.
+    ///
+    /// # Returns
+    /// The pointer to the allocated instance.
     #[inline]
     pub fn create_with(size: usize, vtbl: NonNull<c_void>) -> Option<NonNull<c_void>> {
         use core::alloc::Layout;
@@ -69,17 +92,14 @@ impl BSExtraData {
         use std::alloc::alloc_zeroed;
 
         unsafe {
-            // Step 1: Allocate memory
             let memory = {
                 let layout = Layout::from_size_align(size, align_of::<Self>()).ok()?;
                 NonNull::new(alloc_zeroed(layout))?
             };
 
-            // Step 2: Set the vtable pointer
             let vtable_ptr = memory.cast();
             vtable_ptr.write(vtbl);
 
-            // Step 3: Cast to BSExtraData and return the pointer
             Some(memory.cast())
         }
     }
@@ -104,18 +124,24 @@ impl Default for BSExtraData {
     }
 }
 
+/// Default implemented vtable.
 static BS_EXTRA_DATA_VTBL: BSExtraDataVtbl = BSExtraDataVtbl::new();
 
+/// Virtual function table for `BSExtraData`.
 #[repr(C)]
 pub struct BSExtraDataVtbl {
-    /// C++: `~BSExtraData`
-    CxxDrop: fn(this: &mut BSExtraData),
-    GetType: fn(this: &BSExtraData) -> ExtraDataType,
-    IsNotEqual: fn(this: &BSExtraData, rhs: &BSExtraData) -> bool,
+    /// Destructor (`~BSExtraData` in C++).
+    pub CxxDrop: fn(this: &mut BSExtraData),
+
+    /// Gets the type of extra data.
+    pub GetType: fn(this: &BSExtraData) -> ExtraDataType,
+
+    /// Checks inequality between two `BSExtraData` instances.
+    pub IsNotEqual: fn(this: &BSExtraData, rhs: &BSExtraData) -> bool,
 }
 
 impl BSExtraDataVtbl {
-    /// Create a new default virtual function table
+    /// Creates a new default virtual function table with no-op functions.
     #[inline]
     const fn new() -> Self {
         const fn CxxDrop(_this: &mut BSExtraData) {}
@@ -132,22 +158,29 @@ impl BSExtraDataVtbl {
     }
 }
 
+/// Iterator over `BSExtraData` nodes.
 pub struct BSExtraDataIter<'a> {
+    /// Pointer to the current node.
     pub(crate) current: *mut BSExtraData,
+
+    /// Pointer to the previous node.
     pub(crate) prev: *mut BSExtraData,
-    // Zero size type for giving a lifetime annotation for the compiler
-    // to detect the duration of the pointer.
+
+    /// Lifetime marker for Rust's borrow checker.
     marker: PhantomData<&'a BSExtraData>,
 }
 
 impl BSExtraDataIter<'_> {
+    /// Creates a new iterator starting at `start`.
     #[inline]
     pub const fn new(start: *mut BSExtraData) -> Self {
         Self { current: start, prev: std::ptr::null_mut(), marker: PhantomData }
     }
 
-    /// Deletes the current node and concatenates prev and next
-    /// Return value: pointer to the deleted node
+    /// Removes the current node and concatenates the previous and next nodes.
+    ///
+    /// # Returns
+    /// Pointer to the removed node.
     pub fn remove_current(&mut self) -> Option<*mut BSExtraData> {
         if self.current.is_null() {
             return None;
@@ -185,21 +218,21 @@ impl Iterator for BSExtraDataIter<'_> {
     }
 }
 
+/// Mutable iterator over `BSExtraData` nodes.
 pub struct BSExtraDataIterMut<'a> {
     cur: *mut BSExtraData,
     prev: *mut BSExtraData,
-    // Zero size type for giving a lifetime annotation for the compiler
-    // to detect the duration of the pointer.
     marker: PhantomData<&'a BSExtraData>,
 }
 
 impl BSExtraDataIterMut<'_> {
+    /// Creates a new iterator starting at `start`.
     #[inline]
     pub const fn new(start: *mut BSExtraData) -> Self {
         Self { cur: start, prev: std::ptr::null_mut(), marker: PhantomData }
     }
 
-    /// Delete current node to free memory, concatenate prev and next
+    /// Removes the current node and concatenates the previous and next nodes.
     pub fn remove_current(&mut self) {
         if self.cur.is_null() {
             return;
@@ -214,7 +247,7 @@ impl BSExtraDataIterMut<'_> {
         let to_delete = self.cur;
         self.cur = next;
 
-        // NOTE: UB probably occurs if pointer is not made from heap allocation
+        // Drop the node, assuming it was heap-allocated.
         drop(unsafe { Box::from_raw(to_delete) });
     }
 }
@@ -239,6 +272,7 @@ impl Iterator for BSExtraDataIterMut<'_> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_bs_extra_data_creation() {
         let data = BSExtraData::new();
