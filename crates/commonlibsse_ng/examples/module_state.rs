e488b4345999cc8d47a83848e7718db3419fad73
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-03-28 19:00:33 +0900

    feat(example): add message handling for PostLoadGame event and record game date

--- crates/commonlibsse_ng/examples/module_state.rs
+++ crates/commonlibsse_ng/examples/module_state.rs
@@ -1,3 +1,5 @@
+use commonlibsse_ng::skse::interfaces::messaging::MessageType;
+
 #[commonlibsse_ng::skse_plugin_main]
 fn plugin_main() {
     match commonlibsse_ng::skse::api::get_messaging_interface() {
@@ -5,6 +7,12 @@ fn plugin_main() {
             if let Err(err) = messaging.register_skse_listener(|message| {
                 #[cfg(feature = "tracing")]
                 tracing::info!("SKSE event: {message:#?}");
+
+                if let Some(msg_type) = message.msg_type.to_enum() {
+                    if msg_type == MessageType::PostLoadGame {
+                        record_game_date();
+                    }
+                }
             }) {
                 #[cfg(feature = "tracing")]
                 tracing::error!("{err}");
@@ -17,7 +25,13 @@ fn plugin_main() {
     }
 }
 
-#[commonlibsse_ng::relocate_fn(se_id = "100", ae_id = "100")]
-fn init_extra_data_list(enable: bool) -> bool {
-    type S = SelfSignature;
+fn record_game_date() {
+    use commonlibsse_ng::re::Calendar::Calendar;
+
+    if let Some(calendar) = unsafe { Calendar::get_singleton().as_ref() } {
+        if let Some(date) = calendar.get_time() {
+            #[cfg(feature = "tracing")]
+            tracing::trace!("{date}");
+        };
+    };
 }
