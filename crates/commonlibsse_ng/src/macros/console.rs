3f73e49887fe7d83f6e62a095dccdd2bd9c6e2a6
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-04-11 22:06:30 +0900

    refacotr: move console printing macros to macro module file

---
+++ crates/commonlibsse_ng/src/macros/console.rs
@@ -0,0 +1,50 @@
+//! Skyrim console print
+
+/// Prints to the in-game console (Skyrim) without a newline.
+///
+/// This is analogous to [`print!`](https://doc.rust-lang.org/std/macro.print.html),
+/// but targets the Skyrim in-game console instead of stdout.
+///
+/// # Examples
+///
+/// ```
+/// use commonlibsse_ng::console_print;
+///
+/// console_print!("Hello, {}!", "Dragonborn");
+/// ```
+#[macro_export]
+macro_rules! console_print {
+    ($($arg:tt)*) => {{
+        use ::core::fmt::Write as _;
+        $crate::re::Console::print_fmt(format_args!($($arg)*));
+    }};
+}
+
+/// Prints to the in-game console (Skyrim) with a newline.
+///
+/// This is analogous to [`println!`](https://doc.rust-lang.org/std/macro.println.html),
+/// but targets the Skyrim in-game console instead of stdout.
+///
+/// # Examples
+///
+/// ```no_run
+/// use commonlibsse_ng::console_println;
+///
+/// console_println!("Level up! You are now level {}", 42);
+/// console_println!(); // just a newline
+/// ```
+///
+/// # Allocate Heap Memory
+/// This uses the [`Write`] trait internally, and since [`str`] is a [`CString`], heap allocation occurs each time it is used.
+///
+/// If the string is fixed at compile time, it is better to use `c""` and `Console::print` method to save memory and speed up the process.
+#[macro_export]
+macro_rules! console_println {
+    () => {unsafe {
+        $crate::re::Console::Console::get_singleton_mut()
+            .map(|console| console.print(c"\n".as_ptr()));
+    }};
+    ($($arg:tt)*) => {{
+        $crate::re::Console::print_fmt(format_args!("{}\n", format_args!($($arg)*)));
+    }};
+}
--- crates/commonlibsse_ng/src/macros/mod.rs
+++ crates/commonlibsse_ng/src/macros/mod.rs
@@ -1,2 +1,4 @@
 #[macro_use]
 mod assert;
+#[macro_use]
+mod console;
